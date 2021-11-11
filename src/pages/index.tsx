09939cd376085febaf033b3e54223da0f70f7191
Hugo Vinicius <hugovinicius145@gmail.com>
2021-11-11 10:06:58 -0300

    Textos da pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { Header } from '../components/Header';
 import { Banner } from '../components/Banner';
@@ -11,8 +11,13 @@ export default function Home() {
 
       <Flex w="100%" maxWidth={1480} mx="auto" direction="column">
         <Banner />
-
         <TravelTypes />
+
+        <Flex direction="column" align="center" justify="center" py="8"  color="darkHeading">
+          <Text fontSize="36" fontWeight="500" lineHeight="54px">Vamos nessa</Text>
+          <Text fontSize="36" fontWeight="500" lineHeight="54px">Então escolha seu continente</Text>
+        </Flex>
+
       </Flex>
     </Flex>
   )
